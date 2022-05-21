82ec9bbc5d53a5f858efdb9fc50bf1b41c82c9ab
Akshay KM <akshay194@gmail.com>
2022-05-21 22:24:13 +0530

    responsive design added

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@mui/icons-material'
 import React from 'react'
 
 function SearchComponent(props) {
@@ -7,16 +8,18 @@ function SearchComponent(props) {
     console.log(props.city)
   }
   return (
-        <div className=' bg-slate-200 sm:w-96 w-72 h-96 shadow-2xl rounded-xl p-4'>
+        <div className=' bg-slate-200  w-96 h-96 shadow-2xl rounded-xl p-4'>
           <div className=' bg-slate-200 h-1/5 flex justify-center items-center'>
           <h1 className=' font-bold text-3xl text-gray-500'>Weather App</h1>
           </div>
           <form onSubmit={props.fetchWeather} className='bg-slate-200 h-1/5 flex justify-center items-center'>
-          <input className='pl-2'  type="text" value={props.city}  placeholder='search city' onChange={cityChangeHandler} />
-          <button className=' bg-sky-400 w-20 rounded-r-sm hover:bg-sky-500 text-white font-normal'>Search</button>
+          <input className=' bg-slate-100 pl-2 h-9 rounded-l-md'  type="text" value={props.city}  placeholder='search city' onChange={cityChangeHandler} />
+          <button className=' bg-sky-400 w-12 h-9 rounded-r-sm hover:bg-sky-500 text-white font-normal'>
+            <Search/>
+          </button>
           </form>
           <div className=' bg-slate-200 h-3/5 flex justify-center items-center'>
-          <img className=' w-64 h-48 rounded-3xl' src="https://st2.depositphotos.com/2697407/8436/v/600/depositphotos_84362514-stock-illustration-low-poly-weather-icon-forecast.jpg" alt="asdfasdf" />
+          <img className=' w-96 h-48 rounded-3xl' src="https://images.pond5.com/glowing-neon-line-sun-and-footage-139642648_iconl.jpeg" alt="asdfasdf" />
           </div>
         </div>
   )
