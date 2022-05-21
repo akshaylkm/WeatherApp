fa5a5ec4c8e7c27f7327d38635f93e8722b50c26
Akshay KM <akshay194@gmail.com>
2022-05-21 22:42:40 +0530

    responsive bug fix

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -8,12 +8,12 @@ function SearchComponent(props) {
     console.log(props.city)
   }
   return (
-        <div className=' bg-slate-200   sm:w-96 w-64 h-96 shadow-2xl rounded-xl p-4'>
+        <div className=' bg-slate-200   sm:w-96 w-72 h-96 shadow-2xl rounded-xl p-4'>
           <div className=' bg-slate-200 h-1/5 flex justify-center items-center'>
           <h1 className=' font-bold text-3xl text-gray-500'>Weather App</h1>
           </div>
           <form onSubmit={props.fetchWeather} className='bg-slate-200 h-1/5 flex justify-center items-center'>
-          <input className=' bg-slate-100 pl-2 h-9 rounded-l-md'  type="text" value={props.city}  placeholder='search city' onChange={cityChangeHandler} />
+          <input className=' bg-slate-100 pl-2 h-9 sm:w-62 w-52 rounded-l-md'  type="text" value={props.city}  placeholder='search city' onChange={cityChangeHandler} />
           <button className=' bg-sky-400 w-12 h-9 rounded-r-sm hover:bg-sky-500 text-white font-normal'>
             <Search/>
           </button>
